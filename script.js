4474b47a78f967a94d75baeb99f6d16678989e57
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-09-21 17:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-let table = document.getElementsByTagName('table')[0];
-let price = document.querySelectorAll('.price');
-let totalPrice = document.createElement('tr');
-const getSum = () => {
-    //Add your code here
-    totalPrice.remove();
-    
-        let sum = 0;
-    
-     //let price = document.querySelectorAll('.price');
-     for(let i = 0 ; i < price.length; i++){
-        //console.log(typeof(price[i].innerHTML));
-         sum = sum + parseInt(price[i].innerText); 
-     }
-   
-     totalPrice.innerHTML = ` <td>Total Price</td>
-           <td>${sum}</td>`
-      table.append(totalPrice);
-      sum = 0;
-     
-    }
+let table= document.getElementById("table");
+
+const getSum = (event) => {
+//Add your code here
+	event.preventDefault();
+	  var table = document.getElementById("table"), sumVal = 0;
+            
+            for(var i = 1; i < table.rows.length; i++)
+            {
+                sumVal = sumVal + parseInt(table.rows[i].cells[1].innerHTML);
+            }
+	let newRow= document.createElement("tr");
+	let newCell= document.createElement("td");
+	let newCell2= document.createElement("td");
+	newCell2.textContent= "Sum";
+	newCell.textContent= sumVal;
+	newRow.append(newCell2,newCell);
+	table.appendChild(newRow)
+	
+	 // document.getElementById("val").innerHTML = "Sum Value = " + sumVal;
+            console.log(sumVal); 
+            
+  
+};
+
+
 
 getSumBtn.addEventListener("click", getSum);
