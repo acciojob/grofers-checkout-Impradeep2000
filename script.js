117ae5b73283e35356d11751e5acce3e4077259f
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-09-21 16:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const table=document.getElementsByTagName('table')[0];
+	const tdArr=document.querySelectorAll('.price')
+	let sum=0;
+    tdArr.forEach((ele)=>{
+        console.log(ele.innerText);
+        sum+=Number.parseInt(ele.innerText);
+    })
+   
+	const tr=document.createElement('tr');
+	const td=document.createElement('td');
+    td.innerText=sum;
+	tr.appendChild(td);
+	table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
