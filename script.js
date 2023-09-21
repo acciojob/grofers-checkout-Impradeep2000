48ae94a6e9c0293dc8d2eb910736e86c0ea48a4b
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-09-21 17:07:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 const getSumBtn = document.createElement("button");
+const item=document.querySelectorAll('.item');
+const price=document.querySelectorAll('.price');
+
+
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-let table= document.getElementById("table");
 
-const getSum = (event) => {
+const getSum = () => {
 //Add your code here
-	event.preventDefault();
-	  var table = document.getElementById("table"), sumVal = 0;
-            
-            for(var i = 1; i < table.rows.length; i++)
-            {
-                sumVal = sumVal + parseInt(table.rows[i].cells[1].innerHTML);
-            }
-	let newRow= document.createElement("tr");
-	let newCell= document.createElement("td");
-	let newCell2= document.createElement("td");
-	newCell2.textContent= "Sum";
-	newCell.textContent= sumVal;
-	newRow.append(newCell2,newCell);
-	table.appendChild(newRow)
-	
-	 // document.getElementById("val").innerHTML = "Sum Value = " + sumVal;
-            console.log(sumVal); 
-            
-  
+  let totalPrice=0;
+	price.forEach(priceElement=>{
+		totalPrice+=parseInt(priceElement.textContent);
+	});
+    const row=document.createElement('tr');
+	const totalPriceCell=document.createElement('td');
+	totalPriceCell.textContent="totalPrice";
+	totalPriceCell.colspan=1;
+
+	const totalPriceAmountCell=document.createElement('td');
+	totalPriceAmountCell.textContent=totalPrice;
+
+	row.appendChild(totalPriceCell);
+	row.appendChild(totalPriceAmountCell);
+
+	const tableBody=document.querySelector('table tbody');
+	tableBody.appendChild(row);
 };
-
-
 
 getSumBtn.addEventListener("click", getSum);
